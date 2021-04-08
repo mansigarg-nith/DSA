8a002abf111423c39d3249dc0d6caabdcecc5c91
mansigarg-nith <56781938+mansigarg-nith@users.noreply.github.com>
2021-04-08 19:43:50 +0530

    Add files via upload

---
+++ numbers/seiveoferotusthenes.cpp
@@ -0,0 +1,25 @@
+// find prime numbers in given range
+#include <iostream>
+using namespace std;
+void primeSeive(int n){
+    int prime[100] = {0};
+    for(int i=2; i<=n; i++){
+        if(prime[i]==0){
+            for(int j=i*i; j<=n; j+=i){
+                prime[j] = 1;
+            }
+        }
+    }
+    for(int i=2; i<=n; i++){
+        if(prime[i]==0){
+            cout<<i<<endl;
+        }
+    }
+}
+
+int main(){
+    int n;
+    cin>>n;
+    primeSeive(n);
+    return 0;
+}
